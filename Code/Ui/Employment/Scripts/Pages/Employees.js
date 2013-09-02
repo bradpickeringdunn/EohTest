9993b750f8d086fc8240c70c09c146c88cee8670
Brad Pickering-Dunn <brad.pickeringdunn@pamgolding.co.za>
2013-09-02 16:33:15 +0200

    Added some js candy

--- Code/Ui/Employment/Scripts/Pages/Employees.js
+++ Code/Ui/Employment/Scripts/Pages/Employees.js
@@ -4,13 +4,31 @@ $(document).ready(function () {
 
     $("form").submit(function (e) {
 
-        var employeeId = $(".employeeList").val();
-
-        new Ajax().Call("PUT", "/Employee/DismissEmployee", { employeId: employeeId });
-
-
         e.preventDefault();
+
+        new EmployeeClass().Delete();
     });
 
+    function EmployeeClass() {
+
+        var employeeId = 0;
+
+        this.Delete = function () {
+
+            employeeId = $(".employeeList").val();
+
+            new Ajax().Call("PUT", "/Employee/DismissEmployee", { employeId: employeeId }, this.SuccessfulDelete, this.FailedDelete);
+        }
+
+        this.SuccessfulDelete = function(data){
+            alert("The employee has ben dismissed");
+            
+        }
+
+        this.FailedDelete = function (xhr, data) {
+            alert("There was an error.");
+        }
+
+    }
 
 });
