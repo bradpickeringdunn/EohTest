2f19460fa8f7fcd735859fe625e9e46d5e198b16
Brad Pickering-Dunn <brad.pickeringdunn@pamgolding.co.za>
2013-09-02 11:27:47 +0200

    Added Database project

---
+++ Code/Database/Crud/dbo/Tables/Employee.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Employee] (
+    [EmployeeId]   INT           IDENTITY (1, 1) NOT NULL,
+    [PersonId]     INT           NOT NULL,
+    [EmployeeNum]  NVARCHAR (16) NOT NULL,
+    [EmployeeDate] DATE          NOT NULL,
+    [Terminated]   DATE          NULL,
+    CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED ([EmployeeId] ASC),
+    CONSTRAINT [FK_Employee_Person] FOREIGN KEY ([PersonId]) REFERENCES [dbo].[Person] ([PersonId])
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [IX_Employee]
+    ON [dbo].[Employee]([PersonId] ASC);
+
