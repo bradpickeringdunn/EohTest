2b13c873013c6cf66804875b1dfabad6495580cb
Brad Pickering-Dunn <brad.pickeringdunn@pamgolding.co.za>
2013-09-03 08:46:03 +0200

    Modified package names and assemblies

--- Code/Data/Properties/AssemblyInfo.cs
+++ Code/Data/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Data")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("EOHTest.Data.Crud")]
+[assembly: AssemblyDescription("EOH Data")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Private")]
 [assembly: AssemblyProduct("Data")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
